ab4d5162e1f06673e43897943b841f9d4277ebb8
fredgj <f.gjertsen@gmail.com>
2016-03-07 17:00:46 +0100

    deleted extra line

--- aes.py
+++ aes.py
@@ -92,28 +92,6 @@ def inv_sub_bytes(state):
     return __sub_bytes(state, inv_sbox)
 
 
-def __shift_rows(state,indexes):
-    tmp = state[0][1]
-    state[0][1] = state[1][1]
-    state[1][1] = state[2][1]
-    state[2][1] = state[3][1]
-    state[3][1] = tmp
-
-    tmp = state[0][2]
-    state[0][2] = state[2][2]
-    state[2][2] = tmp
-    tmp = state[1][2]
-    state[1][2] = state[3][2]
-    state[3][2] = tmp
-
-    tmp = state[0][3]
-    state[0][3] = state[3][3]
-    state[3][3] = state[2][3]
-    state[2][3] = state[1][3]
-    state[1][3] = tmp
-
-    return state
-
 # each row in the state is shifted to left
 # with its rownumber.
 def shift_rows(state):
