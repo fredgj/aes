65c4c03ef72520fce45e7dc201a0a432f73e1c49
fredgj <f.gjertsen@gmail.com>
2016-03-11 22:16:50 +0100

    changed keyword arg _format to format_

--- aes_tests.py
+++ aes_tests.py
@@ -8,7 +8,7 @@ class AESTest(unittest.TestCase):
         key = '2b7e151628aed2a6abf7158809cf4f3c'
         block = '6bc1bee22e409f96e93d7e117393172a'
 
-        encrypted = encrypt(block,key, _format='x')
+        encrypted = encrypt(block,key, format_='x')
         expected = '3ad77bb40d7a3660a89ecaf32466ef97'
 
         self.assertEqual(encrypted, expected)
@@ -17,7 +17,7 @@ class AESTest(unittest.TestCase):
         key = '2b7e151628aed2a6abf7158809cf4f3c'
         block = '3ad77bb40d7a3660a89ecaf32466ef97'
 
-        decrypted = decrypt(block,key, _format='x')
+        decrypted = decrypt(block,key, format_='x')
         expected = '6bc1bee22e409f96e93d7e117393172a'
 
         self.assertEqual(decrypted, expected)
@@ -26,7 +26,7 @@ class AESTest(unittest.TestCase):
         key = '8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b'
         block = '6bc1bee22e409f96e93d7e117393172a'
 
-        encrypted = encrypt(block,key, _format='x')
+        encrypted = encrypt(block,key, format_='x')
         expected = 'bd334f1d6e45f25ff712a214571fa5cc'
 
         self.assertEqual(encrypted, expected)
@@ -35,7 +35,7 @@ class AESTest(unittest.TestCase):
         key = '8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b'
         block = 'bd334f1d6e45f25ff712a214571fa5cc'
 
-        decrypted = decrypt(block,key, _format='x')
+        decrypted = decrypt(block,key, format_='x')
         expected = '6bc1bee22e409f96e93d7e117393172a'
 
         self.assertEqual(decrypted, expected)
@@ -44,7 +44,7 @@ class AESTest(unittest.TestCase):
         key = '603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4'
         block = '6bc1bee22e409f96e93d7e117393172a'
 
-        encrypted = encrypt(block,key, _format='x')
+        encrypted = encrypt(block,key, format_='x')
         expected = 'f3eed1bdb5d2a03c064b5a7e3db181f8'
 
         self.assertEqual(encrypted, expected)
@@ -53,7 +53,7 @@ class AESTest(unittest.TestCase):
         key = '603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4'
         block = 'f3eed1bdb5d2a03c064b5a7e3db181f8'
 
-        decrypted = decrypt(block,key, _format='x')
+        decrypted = decrypt(block,key, format_='x')
         expected = '6bc1bee22e409f96e93d7e117393172a'
 
         self.assertEqual(decrypted, expected)
